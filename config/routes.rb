c96d07c893cfc2b4b0e41924280f7630a99338a2
Daniel Sacramento <danisacra@gmail.com>
2020-10-24 16:44:05 -0300

    Added index and show jbuilder

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+
+  namespace :api do
+    namespace :v1 do
+      resources :stores, only: [:index, :show], :defaults => { :format => 'json' }
+    end
+  end
+end
